81b2c7aa69a7c7a098333f80d780b1900600ee89
Olehvedmidskyi <olehvedmidskyi2003@gmail.com>
2020-11-21 21:29:54 +0200

    search2

--- pictures/views.py
+++ pictures/views.py
@@ -9,3 +9,10 @@ class HomePageView(TemplateView):
 class SearchResultsView(ListView):
     model = Pictures
     template_name = 'search_results.html'
+
+    def get_queryset(self): 
+        query = self.request.GET.get('q')
+        object_list = Pictures.objects.filter(
+            Q(name__icontains=query) | Q(culka__icontains=query)
+        )
+        return object_list
